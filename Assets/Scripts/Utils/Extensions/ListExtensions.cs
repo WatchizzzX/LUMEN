5189db7feccd912e7b0c67b7b32456b259f7425e
Zahar Czembrowicz <watchizx@yandex.ru>
2024-02-21 17:39:45 +0300

    feat: Add CleanedListFromNulls

--- Assets/Scripts/Utils/Extensions/ListExtensions.cs
+++ Assets/Scripts/Utils/Extensions/ListExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Utils.Extensions
 {
@@ -7,6 +8,11 @@ namespace Utils.Extensions
         public static void ClearListFromNulls<T>(this List<T> list)
         {
             list.RemoveAll(x => x == null);
-        } 
+        }
+
+        public static List<T> CleanedListFromNulls<T>(this List<T> list)
+        {
+            return list.Where(x => x != null).ToList();
+        }
     }
 }
