17d3332613f5f5ff1c40aa1802327af70e25fd2e
Zahar Czembrowicz <watchizx@yandex.ru>
2024-02-22 11:46:09 +0300

    refactor: Refactor ConnectableComponent

--- Assets/Scripts/LogicalSystem/ConnectableComponent.cs
+++ Assets/Scripts/LogicalSystem/ConnectableComponent.cs
@@ -2,13 +2,29 @@ using UnityEngine;
 
 namespace LogicalSystem
 {
+    /// <summary>
+    /// An abstract class for all elements that should be attached to other elements of LogicalSystem
+    /// </summary>
     public abstract class ConnectableComponent : MonoBehaviour
     {
+        /// <summary>
+        /// ConnectorEvent delegate
+        /// </summary>
         public delegate void ConnectorEvent();
-        public bool Result { protected set; get; }
 
+        /// <summary>
+        /// Event on value changed
+        /// </summary>
         public event ConnectorEvent ValueChanged;
 
+        /// <summary>
+        /// Logical result
+        /// </summary>
+        public bool Result { protected set; get; }
+
+        /// <summary>
+        /// Wrapper for event
+        /// </summary>
         protected void OnValueChanged()
         {
             ValueChanged?.Invoke();
