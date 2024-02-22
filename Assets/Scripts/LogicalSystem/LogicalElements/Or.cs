a58f2e651de231a7952a9f8b2252b253fc2313f8
Zahar Czembrowicz <watchizx@yandex.ru>
2024-02-22 11:47:03 +0300

    refactor: Refactor OrNode

--- Assets/Scripts/LogicalSystem/LogicalElements/Or.cs
+++ Assets/Scripts/LogicalSystem/LogicalElements/Or.cs
@@ -3,6 +3,9 @@ using LogicalSystem.Interfaces;
 
 namespace LogicalSystem.LogicalElements
 {
+    /// <summary>
+    /// Or implementation of LogicalComponent
+    /// </summary>
     public class Or : ILogicalComponent
     {
         public bool Calculate(bool[] inputs)
