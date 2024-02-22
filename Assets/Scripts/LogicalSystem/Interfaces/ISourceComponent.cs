9e8aba7d67ef6332718d31921d315087af097988
Zahar Czembrowicz <watchizx@yandex.ru>
2024-02-22 11:46:30 +0300

    refactor: Refactor ISourceComponent

--- Assets/Scripts/LogicalSystem/Interfaces/ISourceComponent.cs
+++ Assets/Scripts/LogicalSystem/Interfaces/ISourceComponent.cs
@@ -1,13 +1,30 @@
 namespace LogicalSystem.Interfaces
 {
+    /// <summary>
+    /// An interface for elements that can output a value themselves. Work with LogicalSystem
+    /// </summary>
     public interface ISourceComponent
     {
+        /// <summary>
+        /// SourceEvent delegate
+        /// </summary>
         public delegate void SourceEvent(bool state);
 
+        /// <summary>
+        /// Event when internal value changed
+        /// </summary>
         public event SourceEvent OnValueChanged;
-        
+
+        /// <summary>
+        /// Return internal value
+        /// </summary>
+        /// <returns>Internal value</returns>
         public bool GetValue();
 
+        /// <summary>
+        /// Set internal value
+        /// </summary>
+        /// <param name="value">Required value</param>
         public void SetValue(bool value);
     }
 }
