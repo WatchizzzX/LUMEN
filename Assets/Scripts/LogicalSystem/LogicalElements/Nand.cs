8db966ac6b8cf9410c44a038d17f5cd29a8bc15f
Zahar Czembrowicz <watchizx@yandex.ru>
2024-02-22 11:46:48 +0300

    refactor: Refactor NandNode

--- Assets/Scripts/LogicalSystem/LogicalElements/Nand.cs
+++ Assets/Scripts/LogicalSystem/LogicalElements/Nand.cs
@@ -3,6 +3,9 @@ using LogicalSystem.Interfaces;
 
 namespace LogicalSystem.LogicalElements
 {
+    /// <summary>
+    /// Nand implementation of LogicalComponent
+    /// </summary>
     public class Nand : ILogicalComponent
     {
         public bool Calculate(bool[] inputs)
