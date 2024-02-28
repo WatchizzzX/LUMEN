698dd316380633604ecd2b255a1a49101b8d832e
Zahar Czembrowicz <watchizx@yandex.ru>
2024-02-29 00:27:19 +0300

    feat: Add TransitionManager settings

---
+++ Assets/Scripts/Managers/TransitionManagerSettings.cs
@@ -0,0 +1,12 @@
+using EasyTransition;
+using UnityEngine;
+
+namespace Managers
+{
+    [CreateAssetMenu(fileName = "TransitionManagerSettings", menuName = "Settings/Create TransitionManager settings", order = 0)]
+    public class TransitionManagerSettings : ScriptableObject
+    {
+        public GameObject transitionPrefab;
+        public TransitionSettings defaultTransitionSettings;
+    }
+}
