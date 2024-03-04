549bb397aa5ca2b05be342d4ad28cf94a0484ccd
Zahar Czembrowicz <watchizx@yandex.ru>
2024-03-04 09:10:29 +0300

    feat: Now settings for managers are readonly in runtime

--- Assets/Scripts/Managers/Settings/GameManagerSettings.cs
+++ Assets/Scripts/Managers/Settings/GameManagerSettings.cs
@@ -5,6 +5,8 @@ namespace Managers.Settings
     [CreateAssetMenu(fileName = "GameManagerSettings", menuName = "Settings/Create GameManager settings", order = 0)]
     public class GameManagerSettings : ScriptableObject
     {
-        
+        [SerializeField] private float exitCutsceneDuration;
+
+        public float ExitCutsceneDuration => exitCutsceneDuration;
     }
 }
