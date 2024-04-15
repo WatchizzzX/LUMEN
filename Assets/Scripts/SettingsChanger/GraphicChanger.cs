23d2abfe61f7c28d5005dbde011d25d9004153f0
Zahar Czembrowicz <watchizx@yandex.ru>
2024-04-15 18:04:37 +0300

    feat: Add graphic changer script

---
+++ Assets/Scripts/SettingsChanger/GraphicChanger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GraphicChanger
+{
+    public enum GraphicsSettings
+    {
+        Low = 0,
+        Medium = 1,
+        High = 2
+    }
+    
+    public static void SetQuality(GraphicsSettings graphicsSettings)
+    {
+        QualitySettings.SetQualityLevel((int)graphicsSettings);
+    }
+}
