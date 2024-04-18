bed4d7fd18d0200316bf7498ebde2b5a130825c9
Zahar Czembrowicz <watchizx@yandex.ru>
2024-04-18 17:30:43 +0300

    fix: Update DefaultSceneLoader (Editor script)

---
+++ Assets/Scripts/Editor/DefaultSceneLoader/DefaultSceneLoader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace Editor.DefaultSceneLoader
+{
+    [InitializeOnLoad]
+    public static class DefaultSceneLoader
+    {
+        static DefaultSceneLoader()
+        {
+            EditorApplication.playModeStateChanged += ToggleScenes;
+        }
+
+        private static void ToggleScenes(PlayModeStateChange state)
+        {
+            if (!EditorPrefs.GetBool(DefaultScenePrefs.IsActive)) return;
+            var defaultScene =
+                SceneUtility.GetScenePathByBuildIndex(EditorPrefs.GetInt(DefaultScenePrefs.DefaultSceneIndex));
+
+            var currentScenePath = SceneManager.GetActiveScene().path;
+
+            switch (state)
+            {
+                case PlayModeStateChange.ExitingEditMode:
+                {
+                    EditorPrefs.SetString(DefaultScenePrefs.ReturnPath, currentScenePath);
+
+                    if (currentScenePath != defaultScene)
+                    {
+                        if (EditorPrefs.GetBool(DefaultScenePrefs.AutoSave))
+                            EditorSceneManager.SaveOpenScenes();
+                        else
+                            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+
+                        EditorSceneManager.OpenScene(defaultScene);
+                    }
+
+                    break;
+                }
+                case PlayModeStateChange.EnteredEditMode:
+                {
+                    var returnPath = EditorPrefs.GetString(DefaultScenePrefs.ReturnPath);
+
+                    if (currentScenePath != returnPath)
+                        EditorSceneManager.OpenScene(returnPath);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/DefaultSceneLoader/DefaultSceneWindow.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor.DefaultSceneLoader
+{
+    public class DefaultSceneWindow : EditorWindow
+    {
+        private bool isActive;
+        private bool autoSaveOnPlay;
+        private int defaultSceneIndex;
+
+        [MenuItem("Utils/Default Scene Loader Settings")]
+        public static void ShowWindow()
+        {
+            EditorWindow window = GetWindow<DefaultSceneWindow>("Default Scene Loader Settings");
+            window.minSize = new Vector2(200, 100);
+        }
+
+        private void OnLostFocus()
+        {
+            EditorPrefs.SetBool(DefaultScenePrefs.AutoSave, autoSaveOnPlay);
+            EditorPrefs.SetBool(DefaultScenePrefs.IsActive, isActive);
+            EditorPrefs.SetInt(DefaultScenePrefs.DefaultSceneIndex, defaultSceneIndex);
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.LabelField("Default Scene Loader Settings", EditorStyles.boldLabel);
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginVertical("HelpBox");
+            isActive = EditorGUILayout.Toggle("Scene Loader Active", isActive);
+            autoSaveOnPlay = EditorGUILayout.Toggle("Auto Save On Play", autoSaveOnPlay);
+            defaultSceneIndex = EditorGUILayout.IntField("Default Scene Index", defaultSceneIndex);
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
