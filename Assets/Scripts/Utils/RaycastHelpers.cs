05fa4cd5abae25d164d8a99b78982ec38fc7102c
Zahar Czembrowicz <watchizx@yandex.ru>
2024-05-17 12:48:46 +0300

    feat: Add RaycastHelpers

---
+++ Assets/Scripts/Utils/RaycastHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Graphs;
+using UnityEngine;
+
+namespace Utils
+{
+    public static class RaycastHelpers
+    {
+        public static void VisualizeRays(IEnumerable<Ray> rays, Color color, float distance)
+        {
+            foreach (var ray in rays)
+            {
+                VisualizeRay(ray, color, distance);
+            }
+        }
+
+        public static void VisualizeRay(Ray ray, Color color, float distance)
+        {
+            Debug.DrawRay(ray.origin, ray.direction, color, distance, true);
+        }
+
+        public static bool ExecuteAllRays(IEnumerable<Ray> rays, float distance, LayerMask layerMask)
+        {
+            return rays.Any(ray => Physics.Raycast(ray, distance, layerMask));
+        }
+    }
+}
