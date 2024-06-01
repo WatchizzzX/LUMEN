242b62435b3f6e2ce0f420ea4d92a4b9ade6060a
Zahar Czembrowicz <watchizx@yandex.ru>
2024-06-01 16:50:37 +0300

    chore: Add Realtime Cable package

---
+++ Assets/_ImportedAssets/Real-Time Procedural Cable Simple/Scripts/CableRenderer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _ImportedAssets.Real_Time_Procedural_Cable_Simple.Scripts
+{
+    public class BaseCableRenderer : MonoBehaviour
+    {
+        public virtual void RenderCable()
+        {
+            
+        }
+    }
+}
