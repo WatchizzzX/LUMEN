af282aaa70df1f1532f86bdd4098b3784f374016
Zahar Czembrowicz <watchizx@yandex.ru>
2024-06-03 12:05:34 +0300

    chore: Some files update

--- Assets/Baracuda/IL2CPP/TYPE_DEFINITIONS_FOR_IL2CPP.cs
+++ Assets/Baracuda/IL2CPP/TYPE_DEFINITIONS_FOR_IL2CPP.cs
@@ -5,7 +5,7 @@
 //---------- -------------------------- -------
 
 //Runtime Monitoring
-//File generated: 2024-06-02 20:45:47Z
+//File generated: 2024-06-03 11:53:05Z
 //Please dont change the contents of this file. Otherwise IL2CPP runtime may not work with runtime monitoring!
 //Ensure that this file is located in Assembly-CSharp. Otherwise this file may not compile.
 //If this file contains any errors please contact me and/or create an issue in the linked repository.
@@ -13,7 +13,7 @@
 
 #if ENABLE_IL2CPP && !DISABLE_MONITORING
 
-internal class IL2CPP_AOT_99WHV5HIL8CRNLRBDZWV
+internal class IL2CPP_AOT_6IU26TCKUM0QWH035ZI3
 {
     //Value Processor Method Definitions
 
